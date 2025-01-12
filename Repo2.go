023bdd45af0633f8030444df36b9f5740f33d4f1
LenhartS <lenhart.steve@gmail.com>
2025-01-12 17:12:35 +0000

    delete import repo2 from pacakge

--- Repo2.go
+++ Repo2.go
@@ -1,11 +1,13 @@
 package Repo2
 
+//import ("github.com/LenhartS/OutsideRepo3")
+
 func SaySomething2() string {
 	s1 := "String from GolangOutsieRepo2"
 	return s1
 }
 
-func SaySomethingfromRepo3() string {
+func GetSomethingfromRepo3() string {
 	s2 := Repo3Pkg.SaySomething3()
 	return s2
 }
