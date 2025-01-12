99fe2fbee96f919eebafa533ca4f14f75c106d1b
LenhartS <lenhart.steve@gmail.com>
2025-01-12 14:09:02 +0000

    update Repo2.go to remove extra slash at path

--- Repo2.go
+++ Repo2.go
@@ -1,15 +1,11 @@
 package Repo2
 
-import (
-	"github.com/LenhartS/OutsideRepo3/"
-)
-
 func SaySomething2() string {
 	s1 := "String from GolangOutsieRepo2"
 	return s1
 }
 
 func SaySomethingfromRepo3() string {
-	s2 := OutsideRepo3.SaySomething3()
+	s2 := Repo3Pkg.SaySomething3()
 	return s2
 }
