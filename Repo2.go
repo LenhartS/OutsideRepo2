e9faaaf01c110511259785ad7514be80f0dc600c
LenhartS <lenhart.steve@gmail.com>
2025-01-13 20:08:03 +0000

    update Repo2 with import Repo4 and go.mod with get Repo4 from Repo2

--- Repo2.go
+++ Repo2.go
@@ -1,13 +1,13 @@
 package Repo2
 
-//import ("github.com/LenhartS/OutsideRepo3")
+import Repo4 "github.com/LenhartS/OutsideRepo4"
+
+var s4 string
 
 func SaySomething2() string {
-	s1 := "String from GolangOutsieRepo2"
-	return s1
+	println("String from GolangOutsieRepo2")
+	s4 := Repo4.SaySomething4()
+	println("You got a present from Repo4")
+	println(s4, "  <-- this here came from Repo4 to Repo2")
+	return s4
 }
-
-//func GetSomethingfromRepo3() string {
-//	s2 := Repo3Pkg.SaySomething3()
-//	return s2
-//}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/LenhartS/OutsideRepo2
 
 go 1.23.4
 
-require github.com/LenhartS/OutsideRepo3 v1.0.2 // indirect
+require (
+
+	github.com/LenhartS/OutsideRepo4 v1.0.2 // indirect
+)
